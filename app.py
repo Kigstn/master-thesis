eeffd50f94a6fd324728bcc8f6fa331a165c33f8
Daniel <daniel.jaek@gmail.com>
2021-06-15 17:49:19 +0200

    add welcome page add first site visuals

--- app.py
+++ app.py
@@ -1,12 +1,35 @@
-from flask import Flask
+import uvicorn
+import uuid
 
-app = Flask(__name__)
+from fastapi import FastAPI, Request
+from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
+from fastapi.responses import HTMLResponse
 
 
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
+
+# mount the webserver to /static
+app = FastAPI()
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+# load templates
+templates = Jinja2Templates(directory="templates")
 
 
-if __name__ == '__main__':
-    app.run()
+@app.get('/', response_class=HTMLResponse)
+async def root(request: Request):
+    # generate a uuid for the user which will be used to identify them though this experiment
+    user_id = str(uuid.uuid4())
+
+    return templates.TemplateResponse("root.html", {"request": request, "user_id": user_id})
+
+
+@app.get('/test')
+async def root(request: Request):
+    print(request)
+    return "Hi"
+
+
+# start the server
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
