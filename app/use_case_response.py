cd8d47c89b991f5c0f4cbc11a6150000ee2ad523
Daniel <daniel.jaek@gmail.com>
2021-07-26 16:27:05 +0200

    add use case reponses

--- app/use_case_response.py
+++ app/use_case_response.py
@@ -1,3 +1,59 @@
+use_case_response = {
+    1: {
+        1: [
+            "Das kann ja mal passieren. ",
+            "Wir haben dasselbe Modell gerade nicht vorliegend, aber ich könnte ich es für Sie bestellen. Dies würde zwei bis drei Werktage dauern. Ansonsten können Sie sich einen anderen Fernseher hier aussuchen und gleich mitnehmen. Zum Beispiel dieser Fernseher (zeigt auf einen Fernseher). Der ist aber etwas schlechter ausgestattet wie der von Ihnen zuvor bestellte Fernseher. ",
+            "Für einen Aufpreis übernehmen wir außerdem die Lieferung, Montage und Einrichtung. Kann ich Sie für eine verlängerte Garantie oder einen passenden Receiver interessieren?",
+            "Wenn Sie noch Zeit zum Überlegen brauchen, muss ich Sie bitte das wo anders zu tun, denn hinter Ihnen ist schon eine Schlange.",
+        ],
+        2: [
+            "Hallo. Erst einmal tut es mir leid, dass Ihnen das passiert ist, ich verstehe ihre Frustration. ",
+            "Zurzeit ist gibt es einige Lieferprobleme durch die momentane Krise und dadurch können wir nicht immer rechtzeitig liefern, das entschuldigt Ihr Erlebnis aber natürlich nicht. Wenn Sie mir Ihre Bestellnummer geben, kann ich für Sie nachgucken wo, was schiefgelaufen ist, und den Prozess antreiben, das Problem in der Zukunft zu beheben. Danke, dass Sie uns darauf aufmerksam gemacht haben!",
+            "Wir haben dasselbe Modell gerade leider nicht vorliegend, aber ich könnte ich es für Sie bestellen. Dies würde aber ein bis zwei Werktage dauern, es könnte also knapp werden, weshalb ich davon abrate. Ansonsten können Sie sich einen anderen Fernseher hier aussuchen und gleich mitnehmen und das alte Modell natürlich zurückgeben. Zum Beispiel dieser Fernseher (zeigt auf einen Fernseher), der ist quasi der gleich wie der, den Sie bestellen hatten nur von einem anderen Hersteller, aber zum gleichen Preis. Ich benutze den selbst und kann ihn nur empfehlen.",
+            "Da sie schon so viele Probleme mit uns hatten, kann ich Ihnen zusätzlich 20 % Rabatt Gutschein anbieten, als Wiedergutmachung.",
+        ],
+    },
+    2: {
+        1: [
+            "Ich kann Ihnen in diesem Fall folgende Modelle anbieten: (zeigt auf Fernseher) diese wären sofort verfügbar und (zeigt auf andere Fernseher) diese könnte ich bestellen und Ihnen wahrscheinlich im angegebenen Zeitraum organisieren. Die ersten Fernseher könnten Sie sich gleich mitnehmen. Die Fernseher müssen Sie sich aber selbst angucken, denn hinter Ihnen ist schon eine Schlange.",
+            "Für einen Aufpreis übernehmen wir außerdem die Lieferung, Montage und Einrichtung. Kann ich Sie für eine verlängerte Garantie oder einen passenden Receiver interessieren?",
+        ],
+        2: [
+            "Es tut mir leid, dass ihr Fernseher heute kaputt gegangen ist, ich kann gut verstehen, dass dies frustrierend ist. ",
+            "Sie können aber unbesorgt sein, bis in sieben Tagen haben Sie einen neuen Fernseher, dabei helfe ich Ihnen! Wir können uns jetzt gemeinsam einige Modelle in Ihrer Preisvorstellung ansehen, welche Sie direkt mitnehmen können. Dabei kann ich Ihnen die verschiedenen Eigenschaften der Modelle erläutern. Und wenn Sie dabei nichts finden, dann gucke ich noch einmal mit Ihnen in unserem Onlineshop nach, da gibt es noch mal mehr Modelle und die werden innerhalb von ein bis zwei Werktage geliefert. ",
+              "Sie müssen aber auch nicht jetzt sofort etwas kaufen. Wenn Sie wollen, kann ich ein Gerät für Sie zurückstellen und dann können Sie noch eine Nacht drüber schlafen und nach Alternativen gucken.",
+        ],
+    },
+    3: {
+        1: [
+            "Ich kann Ihnen dieses Modell verkaufen, allerdings nicht genau das Ausstellungsstück. Dieses benötigen wir zur Vorführung des Fernsehers. Von diesem Modell haben wir keins mehr auf Lager, ich kann Ihnen dieses Modell allerdings bestellen. Einen anderen Fernseher könnten Sie gleich mitnehmen, aber es ist ja Ihre Sache, was Sie machen wollen. ",
+            "Wenn Sie noch Zeit zum Überlegen brauchen muss, ich Sie bitte das wo anders zu tun, denn hinter Ihnen ist schon eine Schlange.",
+        ],
+        2: [
+            "Schönen guten Tag.",
+            "Es soll also genau dieser Fernseher sein? Oder meinen Sie nur das Modell (Der Verkäufer lacht herzlich)? Ich kann Ihnen dieses Modell verkaufen, allerdings nicht genau das Ausstellungsstück. Dieses benötigen wir zur Vorführung des Fernsehers. Von diesem Modell haben wir keins mehr auf Lager. Durch die Rabattaktion ist unser ganzer Lagerstand leider schon ausverkauft. ",
+            "Ich kann Ihnen dieses Modell allerdings bestellen, die Lieferung wird dann innerhalb von ein bis zwei Tagen zugestellt, das passt also auch noch mit Ihrem Zeitdruck. So würde ich das machen. Sie können sich auch aussuchen, ob der Fernseher direkt zu Ihnen nach Hause oder hier in den Laden geliefert wird. Natürlich kriegen Sie den Rabatt auch bei einer Bestellung. ",
+            "Aktuell haben wir außerdem eine Aktion, dass die Lieferung, Montage und Einrichtung kostenlos sind. Natürlich nur, wenn Sie das wollen. ",
+            "Wenn Sie direkt einen Fernseher mitnehmen wollen, können wir uns jetzt auch gemeinsam einige Modelle in Ihrer Preisvorstellung ansehen, welche Sie direkt mitnehmen können. Dabei kann ich Ihnen die verschiedenen Eigenschaften der Modelle erläutern.",
+        ],
+    },
+    4: {
+        1: [
+            "Den Gutschein können Sie hier einlösen. Ich kann Ihnen dieses Modell verkaufen, allerdings nicht genau das Ausstellungsstück. Dieses benötigen wir zur Vorführung des Fernsehers. Von diesem Modell haben wir keins mehr auf Lager, ich kann Ihnen dieses Modell allerdings bestellen. Einen anderen Fernseher könnten Sie gleich mitnehmen, aber es ist ja Ihre Sache, was Sie machen wollen. ",
+            "Wenn Sie noch Zeit zum Überlegen brauchen, muss ich Sie bitte das wo anders zu tun, denn hinter Ihnen ist schon eine Schlange.",
+        ],
+        2: [
+            "Schönen guten Tag.",
+            "Es soll also genau dieser Fernseher sein? Oder meinen Sie nur das Modell (Der Verkäufer lacht herzlich)? Ich kann Ihnen dieses Modell verkaufen, allerdings nicht genau das Ausstellungsstück. Dieses benötigen wir zur Vorführung des Fernsehers. Von diesem Modell haben wir keins mehr auf Lager. Durch die Rabattaktion ist unser ganzer Lagerstand leider schon ausverkauft. ",
+            "Ich kann Ihnen dieses Modell allerdings bestellen, die Lieferung wird dann innerhalb von ein bis zwei Tagen zugestellt, das passt also auch noch mit Ihrem Zeitdruck. So würde ich das machen. Sie können sich auch aussuchen, ob der Fernseher direkt zu Ihnen nach Hause oder hier in den Laden geliefert wird. Natürlich kriegen Sie den Rabatt auch bei einer Bestellung. ",
+            "Aktuell haben wir außerdem eine Aktion, dass die Lieferung, Montage und Einrichtung kostenlos sind. Natürlich nur, wenn Sie das wollen. ",
+            "Wenn Sie direkt einen Fernseher mitnehmen wollen, können wir uns jetzt auch gemeinsam einige Modelle in Ihrer Preisvorstellung ansehen, welche Sie direkt mitnehmen können. Dabei kann ich Ihnen die verschiedenen Eigenschaften der Modelle erläutern.",
+            "Ach, und den Gutschein können Sie hier natürlich einlösen, das ist gar kein Problem.",
+        ],
+    },
+}
+
+
 # gets the response to the use case and the users emotional state
 def get_use_case_response(use_case_id: int, use_case_step: int) -> str:
-    return "Placeholder"
+    return "\n".join(use_case_response[use_case_id][use_case_step])
